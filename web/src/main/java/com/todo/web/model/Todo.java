d6d3d605139fe5e07f58de8d1c3f9504fefe5c14
Sadik Hasan <sadik.hasan@dsinnovators.com>
2024-04-15 15:36:17 +0600

    added liquibase and lombok

--- web/src/main/java/com/todo/web/model/Todo.java
+++ web/src/main/java/com/todo/web/model/Todo.java
@@ -5,9 +5,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.*;
+
+
 
 @Entity
 @Table(name = "todos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,41 +26,6 @@ public class Todo {
     public Todo(boolean starred, boolean completed, String description) {
         this.starred = starred;
         this.completed = completed;
-        this.description = description;
-    }
-
-    public Todo() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public boolean isStarred() {
-        return starred;
-    }
-
-    public void setStarred(boolean starred) {
-        this.starred = starred;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
         this.description = description;
     }
 }
